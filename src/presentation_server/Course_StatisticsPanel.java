619a9a9d33dd3e0aab7daf7ce7179080b5789a7b
FrankYao <570468837@qq.com>
2016-06-04 17:39:40 +0800

    集成服务器 课程统计的B接口连接

--- src/presentation_server/Course_StatisticsPanel.java
+++ src/presentation_server/Course_StatisticsPanel.java
@@ -1,6 +1,10 @@
 package presentation_server;
 
+import integrated_server.*;
+
 import java.awt.Font;
+import java.util.ArrayList;
+import java.util.Vector;
 
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -13,24 +17,32 @@ import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
 
 public class Course_StatisticsPanel extends JPanel {
-
+	JFrame theFrame ;
+	JTable table;
+	Vector allCoursesData = new Vector();
 	/**
 	 * Create the panel.
 	 */
 	public Course_StatisticsPanel(JFrame theFrame) {
+		this.theFrame = theFrame;
 		this.setBounds(127, 0, 607, 535);
 		this.setLayout(null);
 		this.setVisible(true);
 		theFrame.add(this);
 		
-		JTable table = new JTable();
+		table = new JTable();
 		DefaultTableModel model = new DefaultTableModel();
 		table.setModel(model);
 		table.setFont(new Font("宋体", Font.PLAIN, 13));
 		table.setRowSorter(new TableRowSorter<TableModel>(model));
 		table.setFillsViewportHeight(true);
 		
-		String[] titles = {"学号","姓名","性别","院系"};
+		String[] titles = {"课程编号","课程名称","学分","授课老师","授课地点"};
+		Vector v_titles = new Vector<>();
+		for(int i=0;i<titles.length;i++)
+			v_titles.add(titles[i]);
+		getData();
+		model.setDataVector(allCoursesData, v_titles);
 		
 		JScrollPane scrollPane = new JScrollPane(table);
 		scrollPane.setBounds(22, 80, 558, 402);
@@ -45,6 +57,23 @@ public class Course_StatisticsPanel extends JPanel {
 		JComboBox box = new JComboBox(faculties);
 		box.setBounds(495,35,80,25);
 		add(box);
+	}
+	private void getData() {
+		allCoursesData.clear();
+		// TODO Auto-generated method stub
+		ArrayList<Course> courses = ((StatisticsFrame)theFrame).getCourses();
+		if(courses.size() == 0)
+			return;
+		for(Course c: courses){
+			Vector oneVector = new Vector<>();
+			oneVector.add(c.getId());
+			oneVector.add(c.getName());
+			oneVector.add(c.getScore());
+			oneVector.add(c.getTeacher());
+			oneVector.add(c.getLocation());
+			allCoursesData.add(oneVector);
+		}
+		table.repaint();
 	}
 
 }
--- src/presentation_server/StatisticsFrame.java
+++ src/presentation_server/StatisticsFrame.java
@@ -5,6 +5,11 @@ import integrated_server.*;
 import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
@@ -19,13 +24,14 @@ import javax.swing.UIManager;
 
 import org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper;
 
+import common.FileInformation;
 import B.B_Server.B_Interface;
 
 
 public class StatisticsFrame extends JFrame {
-	private Student_StatisticsPanel studentPanel = new Student_StatisticsPanel(this);
-	private Course_StatisticsPanel coursePanel = new Course_StatisticsPanel(this);
-	private Select_StatisticsPanel selectPanel = new Select_StatisticsPanel(this);
+	private Student_StatisticsPanel studentPanel ; 
+	private Course_StatisticsPanel coursePanel;
+	private Select_StatisticsPanel selectPanel;
 	
 	ArrayList<Student> students = new ArrayList<>();
 	ArrayList<Course> courses = new ArrayList<>();
@@ -37,6 +43,9 @@ public class StatisticsFrame extends JFrame {
 	 */
 	public StatisticsFrame() {
 		startRMI();
+		studentPanel= new Student_StatisticsPanel(this);
+		coursePanel = new Course_StatisticsPanel(this);
+		selectPanel = new Select_StatisticsPanel(this);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(760, 589);
@@ -94,12 +103,12 @@ public class StatisticsFrame extends JFrame {
 	}
 
 	private void startRMI() {
-		String student_ParentFolder = "IServer/student/";
 		// TODO Auto-generated method stub
 		try {
 			BClient = (B_Interface)Naming.lookup("rmi://localhost:8882/B_Interface");
-			
-			//students.addAll(XML_Helper.decodeStudents(fileAddress)BClient.getAllStudents());
+			refreshStudents();
+			refreshCourses();
+			refreshSelections();
 			
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
@@ -112,6 +121,51 @@ public class StatisticsFrame extends JFrame {
 			e.printStackTrace();
 		}
 		
+	}
+
+	public void refreshStudents(){
+		String student_parentFolder = "IServer/statistics/student/";
+		
+		FileInformation student_file = BClient.getAllStudents();
+		printXML(student_parentFolder, student_file);
+		students.clear();
+		students.addAll(XML_Helper.decodeStudents
+				(student_parentFolder+student_file.getName()));
+	}
+	
+	public void refreshCourses(){
+		String course_parentFolder = "IServer/statistics/course/";
+		
+		FileInformation B_course_file = BClient.getAllCourses();
+		printXML(course_parentFolder, B_course_file);
+		courses.clear();
+		courses.addAll(XML_Helper.decodeCourses
+				(course_parentFolder+B_course_file.getName()));
+	}
+	
+	public void refreshSelections(){
+		String selection_parentFolder = "IServer/statistics/selection/";
+
+		FileInformation selection_file = BClient.getAllSelections();
+		
+		printXML(selection_parentFolder, selection_file);
+		selections.clear();
+		selections.addAll(XML_Helper.decodeSelections
+				(selection_parentFolder+selection_file.getName()));
+	}
+	
+	public void printXML(String parentPath, FileInformation fileinfo){
+		 BufferedOutputStream output;
+		try {
+			output = new BufferedOutputStream(new FileOutputStream(new File(parentPath+fileinfo.getName())));
+			output.write(fileinfo.getContent());
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public B_Interface getBClient() {
