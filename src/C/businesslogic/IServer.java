1c9fa7a1645286802a4855cf75f2ac82547a97b5
RAINY <RAINY@gmail.com>
2016-06-07 10:30:25 +0800

    l

---
+++ src/C/businesslogic/IServer.java
@@ -0,0 +1,46 @@
+package C.businesslogic;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+import common.Faculty;
+import common.FileInformation;
+import integrated_server.IServer_Interface;
+
+public class IServer extends UnicastRemoteObject implements IServer_Interface {
+
+	protected IServer() throws RemoteException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String helloWorld() throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FileInformation getSharedCourses(Faculty self) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean selectCourse(FileInformation fromFile, Faculty self) throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean quitCourse(FileInformation fromFile, Faculty self) throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
