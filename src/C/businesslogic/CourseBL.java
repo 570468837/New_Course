4e36b9b82d0999c3411aae0b61aead4a669dc1c4
RAINY <RAINY@gmail.com>
2016-06-11 23:53:13 +0800

    C的获取所有课程实现

--- src/C/businesslogic/CourseBL.java
+++ src/C/businesslogic/CourseBL.java
@@ -6,7 +6,16 @@ import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Vector;
 
+import org.dom4j.Document;
+import org.dom4j.Element;
+
+import B.B_Server.B_XML_Helper;
+import B.BusinessLogicService.IInterface;
+import B.Model.Course;
 import C.businesslogicservice.CourseBLService;
 import C.dataservice.CourseDataService;
 import C.dataservice.StudentDataService;
@@ -16,6 +25,8 @@ import C.po.CoursePO;
  * 获取所有课程，返回的是CoursePO[]
  * 
  * */
+import common.Faculty;
+import common.FileInformation;
 
 public class CourseBL implements CourseBLService{
 	public CoursePO[] showAllCourse() {
@@ -38,6 +49,47 @@ public class CourseBL implements CourseBLService{
 			e.printStackTrace();
 		}
 		return null;
+	}
+	/*
+	 * 这里是用小宇的方法显示出所有的课程，包括本地的和共享的
+	 * 
+	 * */
+	public ArrayList<CoursePO> getAllCourses() {
+		// TODO Auto-generated method stub
+		IInterface iController = IInterface.getInstance() ;
+		ArrayList<CoursePO> result = new ArrayList<CoursePO>();
+		for(int i = 0;i<this.showAllCourse().length;i++ ){
+			result.add(this.showAllCourse()[i]);
+		}
+		try {
+			FileInformation aSharedCourses = iController.IClient.getSharedCourses(Faculty.A) ;
+			FileInformation bSharedCourses = iController.IClient.getSharedCourses(Faculty.B) ;
+			Document aDoc = B_XML_Helper.BytesToDoc(aSharedCourses.getContent()) ;
+			Document cDoc = B_XML_Helper.BytesToDoc(bSharedCourses.getContent()) ;
+			
+			//解析doc获取course对象
+			updateList(result, aDoc) ;
+			updateList(result, cDoc) ;
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result ;
+	}
+	public ArrayList<CoursePO> updateList(ArrayList<CoursePO> list,Document doc){
+		Element root = doc.getRootElement() ;
+		for(Iterator<Element> i=root.elementIterator();i.hasNext();){
+			Element element = i.next() ;
+			Vector<String> strs = new Vector<String>() ;
+			for(Iterator<Element> j=element.elementIterator();j.hasNext();){
+				Element tmp = j.next() ;
+				strs.add(tmp.getStringValue()) ;
+			}
+			CoursePO course = new CoursePO(strs.get(0),strs.get(1),Integer.valueOf(strs.get(2)),Integer.valueOf(strs.get(3)),strs.get(4), strs.get(5),strs.get(6)) ;
+			list.add(course) ;
+		}
+		return list ;
+		
 	}
 	
 /*
